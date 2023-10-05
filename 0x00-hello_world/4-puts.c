1b0b692293b1b5b2724a1550fadb6e06e430cba9
Matt Nyaseda <mattkeneth@gmail.com>
2023-10-05 07:12:32 -0700

    tAskd

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+ * main - prints to standard output
+ * @void : no argument
+ *
+ *
+ *
+ * Return: It returns a value of 0
+ *
+ *
+ */
 int main(void)
 {
-	char name[100] = "Programming is like building a multilingual puzzle";
+	char name[100] = "\"Programming is like building a multilingual puzzle";
 
 	puts(name);
 	return (0);
