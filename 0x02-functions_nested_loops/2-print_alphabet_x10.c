34a5a3c4440cf4aaa9184485b89515eb2c5f7c91
Matt Nyaseda <mattkeneth@gmail.com>
2023-10-11 14:18:21 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - alphabet x10
+ * @void: no arg
+ *
+ * Return: Returns a value of 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char az ;
+
+	while (i <= 9)
+	{
+		az = 'a' ;
+
+		while (az <= 'z')
+		{
+			_putchar(az);
+			az++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
