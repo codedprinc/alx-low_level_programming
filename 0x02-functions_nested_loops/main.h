a9f7bf8039d198044477677ec5a632a13c02352b
Matt Nyaseda <mattkeneth@gmail.com>
2023-10-11 09:20:21 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+#include <unistd.h>
+
+
+/**
+ *_putchar - writes character to c stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+#endif // MAIN_H_INCLUDED
