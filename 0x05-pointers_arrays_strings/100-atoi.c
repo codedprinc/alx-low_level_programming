5e3caf763cd75fc44c3d54e23f520af4190751d1
Matt Nyaseda <mattkeneth@gmail.com>
2023-11-01 12:02:23 -0700

    t

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -5,17 +5,7 @@
  *
  * Return: the converted int
  */
-/*
-1. We initialize a variable i to 1.
-2. We initialize a variable num to 0.
-3. We check if the first character is a minus sign. If it is, we multiply i by -1.
-4. We check if the character is a digit. If it is, we multiply num by 10 and add the digit to it.
-5. We check if num is greater than 0. If it is, we break out of the loop.
-6. We return num * i.
-*/
-/*
-Time Complexity: O(n)
-*/
+
 int _atoi(char *s)
 {
 	int i = 1;
